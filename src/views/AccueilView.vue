5f084aaa89b44d91be989bb146a3e4c7eaa0bfe7
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-27 17:35:54 +0200

    commit init

---
+++ src/views/AccueilView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <h2 class="text-2xl">3 artistes</h2>
+    <h2 class="text-2xl">3 jours</h2>
+    <h2 class="text-2xl">2 scènes</h2>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
