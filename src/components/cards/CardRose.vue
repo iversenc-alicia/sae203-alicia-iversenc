5dc14209f1189a95716e1f607b9d95893d5042b5
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-30 12:22:47 +0200

    card rose valeurs changeantes

--- src/components/cards/CardRose.vue
+++ src/components/cards/CardRose.vue
@@ -1,5 +1,19 @@
+<script>
+export default {
+  /* Les "props" servent à définir les "attributs" qui seront passés a l'instance du composant pour le personnalisé.
+  Chaque "props" a un nom et un type */
+  props: {
+    nom: String,
+    scene: String,
+    heure: String,
+    image: String,
+  },
+  components: {},
+};
+</script>
+
 <template>
-  <div class="relative h-[406px] w-[300px]">
+  <div class="relative">
     <div class="absolute left-[-1px] top-[197px] flex h-52 w-[300px] flex-col items-center justify-center gap-2.5 overflow-hidden">
       <div
         class="
@@ -17,9 +31,9 @@
           py-[50px]
         "
       >
-        <p class="flex-shrink-0 flex-grow-0 text-center text-2xl font-semibold text-gray-50">Martin Garrix</p>
-        <p class="flex-shrink-0 flex-grow-0 text-center text-xl text-gray-50">Scene Plage</p>
-        <p class="flex-shrink-0 flex-grow-0 text-center text-base font-medium text-gray-50">14h30</p>
+        <p class="flex-shrink-0 flex-grow-0 text-center text-2xl font-semibold text-gray-50">{{ nom }}</p>
+        <p class="flex-shrink-0 flex-grow-0 text-center text-xl text-gray-50">scene{{ scene }}</p>
+        <p class="flex-shrink-0 flex-grow-0 text-center text-base font-medium text-gray-50">{{ heure }}</p>
       </div>
     </div>
     <div class="absolute left-[-1px] top-[-1px] flex h-[226px] w-[300px] flex-col items-start justify-start gap-2.5 overflow-hidden">
@@ -38,9 +52,11 @@
           <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
             <use xlink:href="#image0_1176_2403" transform="translate(-0.16963) scale(0.00167407 0.00222222)"></use>
           </pattern>
-          <image id="image0_1176_2403" width="800" height="450"></image>
         </defs>
       </svg>
+      <img :src="image" alt="artiste" width="800" height="450" />
     </div>
   </div>
 </template>
+
+
--- src/views/Programmation.vue
+++ src/views/Programmation.vue
@@ -1,1 +1,12 @@
-<template></template>
+<template>
+  <div>
+    <card-rose nom="ttt" scene="dbdgb" heure="erfger" image="/img/djsnake-fonce.webp" />
+  </div>
+</template>
+
+<script>
+import CardRose from "../components/cards/CardRose.vue";
+export default {
+  components: { CardRose },
+};
+</script>
