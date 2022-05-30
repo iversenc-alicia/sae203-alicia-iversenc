9450cf992f2e537e8a73e277cab459e42ab81ac6
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-30 21:48:27 +0200

    boutons accueil

--- src/components/Bouton.vue
+++ src/components/Bouton.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="relative flex max-w-md justify-center gap-2.5 rounded-[3px] border border-pink-600 px-3 py-2">
-    <p class="flex-shrink-0 flex-grow-0 text-center text-2xl font-semibold text-pink-600">BOUTON</p>
+    <p class="flex-shrink-0 flex-grow-0 text-center text-2xl font-semibold text-pink-600">{{ nom }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    nom: String,
+  },
+  components: {},
+};
+</script>
+
--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -13,29 +13,132 @@
       <img src="/public/img/djsnake-clair.webp" class="flex-grow self-stretch object-cover" />
       <img src="/public/img/marshmello-clair.webp" class="flex-grow self-stretch object-cover" />
     </div>
-    <Bouton />
+    <a href="./Artistes"><bouton class="mt-5 mb-10" nom="Découvrir tous les artistes"> </bouton></a>
+
     <div class="grid grid-cols-4 text-4xl">
       <div class="float-left my-auto mx-auto">
         <h2 class="font-bold text-gray-50">Un programme complet</h2>
       </div>
+
       <div class="pb-2 text-center text-2xl font-semibold">
         <h3>Vendredi 8 juillet</h3>
-        <ProgrammeArtistes />
+        <div class="relative flex h-[245px] w-[300px] flex-col items-start justify-start gap-2.5">
+          <div class="flex-shrink-0 flex-grow-0 self-stretch">
+            <svg
+              width="300"
+              height="245"
+              viewBox="0 0 300 245"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute left-[-0.5px] top-[-0.5px]"
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <path
+                d="M0 1.07985C0 0.49735 0.495883 0.0381302 1.07667 0.0827888L299.077 22.9971C299.598 23.0371 300 23.4716 300 23.9941V243.92C300 244.503 299.504 244.962 298.923 244.917L0.923324 221.999C0.40232 221.959 0 221.524 0 221.002L0 1.07985Z"
+                fill="url(#paint0_linear_1111_843)"
+              ></path>
+              <defs>
+                <linearGradient id="paint0_linear_1111_843" x1="150" y1="0" x2="150" y2="245" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#DB2777"></stop>
+                  <stop offset="0.560417" stop-color="#DB2777" stop-opacity="0.517708"></stop>
+                  <stop offset="1" stop-color="#DB2777" stop-opacity="0"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+            <p class="absolute left-0 top-[59px] w-[300px] text-center text-2xl font-semibold text-gray-50">
+              <span class="w-[300px] text-center text-2xl font-semibold text-gray-50">Sikdope</span><br />Joel Corry<span
+                class="w-[300px] text-center text-2xl font-semibold text-gray-50"
+              ></span
+              ><br />Timmy Trumpet<span class="w-[300px] text-center text-2xl font-semibold text-gray-50"></span><br /><span
+                class="w-[300px] text-center text-2xl font-semibold text-gray-50"
+                >Don diablo</span
+              >
+            </p>
+          </div>
+        </div>
       </div>
       <div class="text-center text-2xl font-semibold">
         <h3>Samedi 9 juillet</h3>
-        <ProgrammeArtistes />
+        <div class="relative flex h-[245px] w-[300px] flex-col items-start justify-start gap-2.5">
+          <div class="flex-shrink-0 flex-grow-0 self-stretch">
+            <svg
+              width="300"
+              height="245"
+              viewBox="0 0 300 245"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute left-[-0.5px] top-[-0.5px]"
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <path
+                d="M0 1.07985C0 0.49735 0.495883 0.0381302 1.07667 0.0827888L299.077 22.9971C299.598 23.0371 300 23.4716 300 23.9941V243.92C300 244.503 299.504 244.962 298.923 244.917L0.923324 221.999C0.40232 221.959 0 221.524 0 221.002L0 1.07985Z"
+                fill="url(#paint0_linear_1111_843)"
+              ></path>
+              <defs>
+                <linearGradient id="paint0_linear_1111_843" x1="150" y1="0" x2="150" y2="245" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#DB2777"></stop>
+                  <stop offset="0.560417" stop-color="#DB2777" stop-opacity="0.517708"></stop>
+                  <stop offset="1" stop-color="#DB2777" stop-opacity="0"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+            <p class="absolute left-0 top-[59px] w-[300px] text-center text-2xl font-semibold text-gray-50">
+              <span class="w-[300px] text-center text-2xl font-semibold text-gray-50">MArtin Garrix</span><br />Gabry Ponte<span
+                class="w-[300px] text-center text-2xl font-semibold text-gray-50"
+              ></span
+              ><br />DJ Snake<span class="w-[300px] text-center text-2xl font-semibold text-gray-50"></span><br /><span
+                class="w-[300px] text-center text-2xl font-semibold text-gray-50"
+                >Showtek</span
+              >
+            </p>
+          </div>
+        </div>
       </div>
       <div class="text-center text-2xl font-semibold">
         <h3>Dimanche 10 juillet</h3>
-        <ProgrammeArtistes />
+        <div class="relative flex h-[245px] w-[300px] flex-col items-start justify-start gap-2.5">
+          <div class="flex-shrink-0 flex-grow-0 self-stretch">
+            <svg
+              width="300"
+              height="245"
+              viewBox="0 0 300 245"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute left-[-0.5px] top-[-0.5px]"
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <path
+                d="M0 1.07985C0 0.49735 0.495883 0.0381302 1.07667 0.0827888L299.077 22.9971C299.598 23.0371 300 23.4716 300 23.9941V243.92C300 244.503 299.504 244.962 298.923 244.917L0.923324 221.999C0.40232 221.959 0 221.524 0 221.002L0 1.07985Z"
+                fill="url(#paint0_linear_1111_843)"
+              ></path>
+              <defs>
+                <linearGradient id="paint0_linear_1111_843" x1="150" y1="0" x2="150" y2="245" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#DB2777"></stop>
+                  <stop offset="0.560417" stop-color="#DB2777" stop-opacity="0.517708"></stop>
+                  <stop offset="1" stop-color="#DB2777" stop-opacity="0"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+            <p class="absolute left-0 top-[59px] w-[300px] text-center text-2xl font-semibold text-gray-50">
+              <span class="w-[300px] text-center text-2xl font-semibold text-gray-50">Marshmello</span><br />KSHMR<span
+                class="w-[300px] text-center text-2xl font-semibold text-gray-50"
+              ></span
+              ><br />Mike Candyst<span class="w-[300px] text-center text-2xl font-semibold text-gray-50"></span><br /><span
+                class="w-[300px] text-center text-2xl font-semibold text-gray-50"
+                >Nicky Romero</span
+              >
+            </p>
+          </div>
+        </div>
       </div>
     </div>
+    <a href="./Programmation"><bouton class="mt-5 mb-10" nom="Découvrir toute la programmation"> </bouton></a>
 
     <div class="grid grid-cols-2">
       <h2 class="my-auto mx-auto text-4xl font-bold">Découvrir le festival</h2>
       <FestivalImage />
     </div>
+    <a href="./Festival"><bouton class="mt-5 mb-10" nom="Découvrir toute le concept"> </bouton></a>
   </main>
 </template>
 
