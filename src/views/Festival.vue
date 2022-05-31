933f68d4d7c39481de15491c22bf2cf5067d8767
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-31 09:28:20 +0200

    Page festival responsive

--- src/views/Festival.vue
+++ src/views/Festival.vue
@@ -5,7 +5,7 @@
 
   <h2 class="mb-10 ml-8 text-4xl font-bold">A propos de l'Electro Bancal</h2>
   <div class="mx-10">
-    <div class="grid grid-cols-2">
+    <div class="grid grid-cols-1 lg:grid-cols-2">
       <p class="my-auto p-5 text-base">
         Le festival electro Bancal c’est avant tout de la bonne musique. 12 artistes piliers du monde de la musique électro réunis sur 3
         jours. Parmi eux on peut trouver Martin Garrix, MArshmello ou encore DJ Snake. Le genre de musique privilégié est EDM (Electronic
@@ -21,20 +21,20 @@
       </p>
     </div>
   </div>
-  <h2 class="mt-20 ml-8 mb-10 text-4xl font-bold">Le lac de la Bancalié</h2>
+  <h2 class="mt-20 mb-10 ml-8 mr-8 text-4xl font-bold">Le lac de la Bancalié</h2>
   <div class="gp-5 mx-auto grid w-10/12 grid-cols-2">
-    <img class="" src="/public/img/bancalie1.webp" alt="" />
-    <img class="" src="/public/img/bancalie2.webp" alt="" />
+    <img class="" src="/public/img/bancalie1.webp" alt="photo du lac de la Bancalié" />
+    <img class="" src="/public/img/bancalie2.webp" alt="photo du lac de la Bancalié" />
   </div>
   <div class="mx-auto grid w-10/12 grid-cols-1">
-    <img src="/public/img/bancalie3.webp" alt="" />
+    <img src="/public/img/bancalie3.webp" alt="photo du lac de la Bancalié" />
   </div>
 
   <h2 class="ml-8 mt-20 mb-10 text-4xl font-bold">Deux scènes</h2>
 
-  <div class="mx-10 grid grid-cols-2">
+  <div class="grids-cols-1 mx-10 grid sm:grid-cols-2">
     <div>
-      <h3 class="mt-20 mb-5 text-center text-2xl">Scène Plage (scène terrestre)</h3>
+      <h3 class="mt-5 mb-5 text-center text-2xl">Scène Plage (scène terrestre)</h3>
       <img class="" src="/public/img/festival-terrestre.webp" alt="" />
     </div>
 
