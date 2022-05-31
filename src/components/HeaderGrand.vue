53837a0861d9b783b57416847c99a625a208ef51
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-31 09:37:46 +0200

    Header desktop

---
+++ src/components/HeaderGrand.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative my-auto grid h-[118px] w-full grid-cols-7">
+    <LogoHeader class="pr-4" />
+    <RouterLink class="text pt-6 text-xl font-bold" to="/">Electro Bancal</RouterLink>
+    <RouterLink class="pt-6 text-xl font-bold" to="/Artistes">Artistes</RouterLink>
+    <RouterLink class="pt-6 text-xl font-bold" to="/Programmation">Programmation</RouterLink>
+    <RouterLink class="pt-6 text-xl font-bold" to="/Festival">Festival</RouterLink>
+    <RouterLink class="pt-6 text-xl font-bold" to="/Concert">Concerts</RouterLink>
+    <RouterLink class="pt-6 text-xl font-bold" to="/Contact">Contact</RouterLink>
+  </div>
+</template>
+
+<script>
+import LogoHeader from "./icons/LogoHeader.vue";
+export default {
+  components: { LogoHeader },
+};
+</script>
