4a99d1c39d7fad509ec88706fa851eb88424d411
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-31 11:35:55 +0200

    footer responsive

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,26 @@ import Bouton from "./components/Bouton.vue";
 import ProgrammeArtistes from "./components/ProgrammeArtistes.vue";
 import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
+import HeaderGrand from "./components/HeaderGrand.vue";
+import FooterDeux from "./components/FooterDeux.vue";
 
 export default {
-  components: { Bouton, ProgrammeArtistes, Footer, Header, RouterView, RouterLink },
+  components: { Bouton, ProgrammeArtistes, Footer, Header, RouterView, RouterLink, HeaderGrand, FooterDeux },
 };
 </script>
 
 <template>
   <header class="text-gray-50">
-    <Header />
+    <Header class="lg:hidden" />
+    <header-grand class="hidden lg:flex" />
   </header>
   <main>
     <RouterView />
   </main>
 
   <footer>
-    <Footer />
+    <footer-deux class="lg:hidden" />
+    <Footer class="hidden sm:flex" />
   </footer>
 </template>
 
---
+++ src/components/FooterDeux.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="mt-40 flex w-full flex-col items-center justify-center gap-12 overflow-hidden bg-cyan-600 py-8">
+    <div class="">
+      <h3 class="mb-5 text-center text-xl font-bold">Inscris-toi à la Newsletter d’Electro Bancal !</h3>
+
+      <div class="mx-3">
+        <label class="" for="grid-first-name"></label>
+        <input
+          class="
+            mb-3
+            block
+            w-full
+            appearance-none
+            rounded-lg
+            border border-gray-500
+            bg-slate-50
+            py-3
+            px-4
+            leading-tight
+            text-gray-500
+            focus:bg-slate-300 focus:outline-none
+          "
+          id="grid-first-name"
+          type="text"
+          placeholder="adressemail@mail.com"
+        />
+        <div class="gap-2.5 rounded-[3px] bg-fuchsia-900 py-2">
+          <p class="flex-shrink-0 flex-grow-0 text-center font-semibold text-slate-50 lg:text-2xl">Je m’inscris !</p>
+        </div>
+      </div>
+    </div>
+    <Logo />
+    <div class="grid grid-cols-2 justify-center gap-5">
+      <a class="px-2" href="https://open.spotify.com/"><SpotifyFooter /></a>
+      <a class="px-2" href="https://www.instagram.com/"><InstagramFooter /></a>
+      <a class="px-2" href="https://www.facebook.com/"><FacebookFooter /></a>
+      <a class="px-2" href="https://www.youtube.com/"><YoutubeFooter /></a>
+    </div>
+    <div class="mx-auto grid grid-cols-2 gap-x-10 gap-y-2 text-left font-medium">
+      <a href="../views/Programmation">Programmation</a>
+      <a href="../views/Festival">Festival</a>
+      <a href="../views/Artistes">Artistes</a>
+      <a href="../views/Contact">Contact</a>
+      <a href="../views/MentionsLegales">Mentions légales</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import YoutubeFooter from "./icons/YoutubeFooter.vue";
+import FacebookFooter from "./icons/FacebookFooter.vue";
+import InstagramFooter from "./icons/InstagramFooter.vue";
+import SpotifyFooter from "./icons/SpotifyFooter.vue";
+import Logo from "./icons/Logo.vue";
+export default {
+  components: { YoutubeFooter, FacebookFooter, InstagramFooter, SpotifyFooter, Logo },
+};
+</script>
