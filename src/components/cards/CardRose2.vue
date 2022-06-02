677dfef377e14728c1a98257d07aa6a0afe63a9e
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-06-02 08:55:10 +0200

    Card artiste page artistes

---
+++ src/components/cards/CardRose2.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  /* Les "props" servent à définir les "attributs" qui seront passés a l'instance du composant pour le personnalisé.
+  Chaque "props" a un nom et un type */
+  props: {
+    nom: String,
+    ecoutes: String,
+    image: String,
+  },
+  components: {},
+};
+</script>
+
+<template>
+  <div class="w-96 max-w-xs bg-gradient-to-b from-pink-600 to-gray-950 lg:max-w-lg xl:max-w-2xl">
+    <div class="container mx-auto">
+      <div class="block">
+        <div class="flex flex-col">
+          <div class="p-4">
+            <img class="aspect-video w-full object-cover" :src="image" alt="artiste" />
+          </div>
+          <figcaption class="p-4 text-center">
+            <h3 class="py-1 text-2xl font-semibold">{{ nom }}</h3>
+            <div class="flex flex-wrap gap-x-3">
+              <img src="/img/Spotify.svg" alt="logo spotify" />
+              <p class="py-1 text-base">{{ ecoutes }}</p>
+            </div>
+          </figcaption>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
--- src/views/Artistes.vue
+++ src/views/Artistes.vue
@@ -6,25 +6,33 @@
     <h2 class="ml-10 mb-8 mt-14 text-4xl">Les incontournables</h2>
     <div class="mx-auto grid grid-cols-1 justify-items-center gap-y-20 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
       <a href="./ArtisteSeul"
-        ><card-rose nom="Martin Garrix" scene="23 067 320 auditeurs par mois" image="/img/martingarrix-fonce.webp"
+        ><card-rose-2 nom="Martin Garrix" ecoutes="23 067 320 auditeurs par mois" image="/img/martingarrix-fonce.webp"
       /></a>
-      <a href="./ArtisteSeul"><card-rose nom="Marshmello" scene="39 040 156 auditeurs par mois" image="/img/marshmello-fonce.webp" /></a>
-      <a href="./ArtisteSeul"><card-rose nom="Gabry Ponte" scene="15 302 759 auditeurs par mois" image="/img/gabryponte-fone.webp" /></a>
-      <a href="./ArtisteSeul"><card-rose nom="DJ Snake" scene="28 818 882 auditeurs par mois" image="/img/djsnake-fonce.webp" /></a>
       <a href="./ArtisteSeul"
-        ><card-rose nom="Timmy Trumpet" scene="10 029 125 auditeurs par mois" image="/img/timmytrumpet-fonce.webp"
+        ><card-rose-2 nom="Marshmello" ecoutes="39 040 156 auditeurs par mois" image="/img/marshmello-fonce.webp"
       /></a>
-      <a href="./ArtisteSeul"><card-rose nom="Joel Corry" scene="21 221 035 auditeurs par mois" image="/img/joelcorry-fonce.webp" /></a>
+      <a href="./ArtisteSeul"
+        ><card-rose-2 nom="Gabry Ponte" ecoutes="15 302 759 auditeurs par mois" image="/img/gabryponte-fone.webp"
+      /></a>
+      <a href="./ArtisteSeul"><card-rose-2 nom="DJ Snake" ecoutes="28 818 882 auditeurs par mois" image="/img/djsnake-fonce.webp" /></a>
+      <a href="./ArtisteSeul"
+        ><card-rose-2 nom="Timmy Trumpet" ecoutes="10 029 125 auditeurs par mois" image="/img/timmytrumpet-fonce.webp"
+      /></a>
+      <a href="./ArtisteSeul"><card-rose-2 nom="Joel Corry" ecoutes="21 221 035 auditeurs par mois" image="/img/joelcorry-fonce.webp" /></a>
     </div>
     <h2 class="ml-10 mb-8 mt-14 text-4xl">A ne pas rater</h2>
     <div class="mx-auto grid grid-cols-1 justify-items-center gap-y-20 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-      <a href="./ArtisteSeul"><card-rose nom="Don Diablo" scene="6 938 751 auditeurs par mois" image="/img/djsnake-fonce.webp" /></a>
-      <a href="./ArtisteSeul"><card-rose nom="Sikdope" scene="685 173 auditeurs par mois" image="/img/sikdope-fonce.webp" /></a>
-      <a href="./ArtisteSeul"><card-rose nom="KSHMR" scene="6 885 032 auditeurs par mois" image="/img/kshmr-fonce.jpg" /></a>
+      <a href="./ArtisteSeul"><card-rose-2 nom="Don Diablo" ecoutes="6 938 751 auditeurs par mois" image="/img/djsnake-fonce.webp" /></a>
+      <a href="./ArtisteSeul"><card-rose-2 nom="Sikdope" ecoutes="685 173 auditeurs par mois" image="/img/sikdope-fonce.webp" /></a>
+      <a href="./ArtisteSeul"><card-rose-2 nom="KSHMR" ecoutes="6 885 032 auditeurs par mois" image="/img/kshmr-fonce.jpg" /></a>
 
-      <a href="./ArtisteSeul"><card-rose nom="Mike Candys" scene="3 500 139 auditeurs par mois" image="/img/mikecandys-fonce.webp" /></a>
-      <a href="./ArtisteSeul"><card-rose nom="Nicky Romero" scene="5 498 167 auditeurs par mois" image="/img/nickyromero-fonce.webp" /></a>
-      <a href="./ArtisteSeul"><card-rose nom="Showtek" scene="4 384 856 auditeurs par mois" image="/img/showtek-fonce.webp" /></a>
+      <a href="./ArtisteSeul"
+        ><card-rose-2 nom="Mike Candys" ecoutes="3 500 139 auditeurs par mois" image="/img/mikecandys-fonce.webp"
+      /></a>
+      <a href="./ArtisteSeul"
+        ><card-rose-2 nom="Nicky Romero" ecoutes="5 498 167 auditeurs par mois" image="/img/nickyromero-fonce.webp"
+      /></a>
+      <a href="./ArtisteSeul"><card-rose-2 nom="Showtek" ecoutes="4 384 856 auditeurs par mois" image="/img/showtek-fonce.webp" /></a>
     </div>
   </main>
 
@@ -34,8 +42,8 @@
 <script>
 import Baniere from "/src/components/Baniere.vue";
 import ArtisteCard from "/src/components/cards/ArtisteCard.vue";
-import CardRose from "/src/components/cards/CardRose.vue";
+import CardRose2 from "/src/components/cards/CardRose2.vue";
 export default {
-  components: { Baniere, ArtisteCard, CardRose },
+  components: { Baniere, ArtisteCard, CardRose2 },
 };
 </script>
